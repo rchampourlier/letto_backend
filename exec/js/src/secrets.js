3f245abcfb2887f2a8b9f28f2097754fce48eed2
Romain Champourlier <public@rchampourlier.com>
2017-10-22 18:52:52 +0200

    Removed secrets
    - Committed by error when the file was moved
    - The Trello token was revoked.

--- exec/js/src/secrets.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-  "trello": {
-    "api_key": "aaf0ba6012f091d3ede839d8988738fe",
-    "api_token": "6da375c5b8472377a021fcddb019e60ad624678cc80adb8712cdc69a7170f163"
-  }
-};
-
-module.exports = credentials;
