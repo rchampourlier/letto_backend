3f4362b212dbcb067215c821ae6d6813ccce496b
Romain Champourlier <public@rchampourlier.com>
2017-10-28 12:35:35 +0200

    Updated SMS alert workflow

--- exec/js/src/workflows/jt/newrelic/sms_alert.js
+++ exec/js/src/workflows/jt/newrelic/sms_alert.js
@@ -1,16 +1,24 @@
 module.exports = (data, secrets, context, console) => {
   var ovh = require('ovh')(secrets.ovh);
-  ovh.request('GET', '/sms', function (err, serviceName) {
+  ovh.request('GET', '/sms', function (err, serviceNames) {
     if (err) {
-      console.log(err, serviceName);
+      console.error(err, serviceName);
     }
     else {
+      if (serviceNames.constructor === Array) {
+        serviceName = serviceNames[0];
+      }
+      else {
+        serviceName = serviceNames;
+      }
+      console.log("Retrieved /sms service name: `" + serviceName + "`");
+      console.log("Posting new SMS");
       ovh.request('POST', '/sms/' + serviceName + '/jobs', {
         message: 'Hello World!',
         senderForResponse: true,
         receivers: [secrets.myPhoneNumber]
       }, function (errsend, result) {
-        console.log(errsend, result);
+        console.error(errsend, result);
       });
     }
   });
